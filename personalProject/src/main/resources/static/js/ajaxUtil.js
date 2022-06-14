38397703eaa536a9623fae3fcdab0a64aaf83598
rudxo <rudxo1588@naver.com>
2022-06-14 22:50:31 +0900

    ajax 공통모듈로 만들어보기

--- personalProject/src/main/resources/static/js/ajaxUtil.js
+++ personalProject/src/main/resources/static/js/ajaxUtil.js
@@ -1,12 +1,22 @@
 /**
  * Ajax 공통 모듈
  */
-var AjaxUtil = {
+function ajax(type, url, data, callback) {
+	$.ajax({
+		url		: url,
+		method	: type,
+		data	: data,
+		dataTyoe 	: 'json',
+		contentType	: false,
+		processData	: false,
+		success	: function(res, result, xhr) {
+			return callback(res, result);
+		},
 
-	ajax = function(url, httpType) {
-		console.log(url, httpType)
-		$.ajax({
-			
-		});
-	}
+		error	: function(xhr, status, error) {
+			console.log('status ' + status)
+			console.log('error ' + error)
+			console.log('xhr ' + xhr)
+		}
+	})
 }
